b4f77cb236f6ae0515c0077b69b0fab8c609d35e
AnnSeidel <annika.seidel@mpibpc.mpg.de>
2018-07-06 13:16:57 +0200

    added typdefs for kmer handling

---
+++ src/kmer.h
@@ -0,0 +1,12 @@
+#ifndef KMER
+#define KMER
+
+#include "uint128.h"
+
+typedef uint128_t       spacedKmerType;
+typedef uint64_t        kmerType;
+
+
+#endif // KMER
+
+
---
+++ src/uint128.h
@@ -0,0 +1,9 @@
+#ifndef UINT128
+#define UINT128
+
+#include <stdint.h>
+
+typedef __uint128_t uint128_t;
+
+#endif // UINT128
+
