5c8aefa6719cc85ef0509f3a55369e09950eeed3
AnnSeidel <annika.seidel@mpibpc.mpg.de>
2020-11-18 21:26:22 +0100

    supress warnings from libraries

--- main.cpp
+++ main.cpp
@@ -21,11 +21,11 @@
 #ifdef GIT_SHA1
 #define str2(s) #s
 #define str(s) str2(s)
-const char *version = str(GIT_SHA1);
+  const char *version = str(GIT_SHA1);
 #undef str
 #undef str2
 #else
-const char *version = "UNKNOWN";
+  const char *version = "UNKNOWN";
 #endif
 
 
